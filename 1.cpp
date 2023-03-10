1983faf5780eb6998313ade9ac0105fa7ade86ee
MuhammadTaha01 <127325759+MuhammadTaha01@users.noreply.github.com>
2023-03-11 00:43:26 +0500

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int height, width, area;
+    height=5;
+    width=4;
+
+    area= height*width;
+    cout<<"Area of square= "<< area;
+
+    return 0;
+}
